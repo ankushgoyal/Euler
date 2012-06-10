2804b4aa00eec00ea8f7f2b356eb5d2b542a625b
Ankush Goyal <ankush.goyal@nextag.com>
2012-06-10 10:39:05 -0500

    Problem 3

---
+++ src/problem3/PrimeFactorization.java
@@ -0,0 +1,46 @@
+package problem3;
+
+import java.util.*;
+
+public class PrimeFactorization {
+	
+	private List<Long> findPrimeFactors(Long no) {
+		List<Long> factors = new ArrayList<Long>();
+		for(long i = 2; i*i <= no; i++) {
+			if(no % i == 0 && isPrime(i)) {
+//				System.out.println(i + "");
+				factors.add(i);
+			}
+		}
+		
+		return factors;
+	}
+	
+	private boolean isPrime(Long no) {
+		if(no == 2) {
+			return true;
+		}
+		
+		if(no % 2 == 0) {
+			return false;
+		}
+		
+		boolean isPrime = true;
+		
+		for(long i = 3; i*i <= no; i+=2) {
+			if(no % i == 0) {
+				return false;
+			}
+		}
+		
+		return isPrime;
+		
+	}
+	
+	public static void main(String[] args) {
+		PrimeFactorization pf = new PrimeFactorization();
+		List<Long> factors = pf.findPrimeFactors(Long.parseLong("600851475143"));
+		System.out.println(factors.get(factors.size()-1));
+	}
+
+}
